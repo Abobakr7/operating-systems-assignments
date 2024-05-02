97231117204f6705ffc342f4a3ac1ae2f824261b
Abobakr <mabobakr79@yahoo.com>
2024-05-02 16:12:37 +0300

    Initial Commit - starting cpu-scheduling

---
+++ cpu-scheduling/src/main/java/app/cpu/scheduling/CpuScheduling.java
@@ -0,0 +1,8 @@
+package app.cpu.scheduling;
+
+public class CpuScheduling extends javax.swing.JFrame {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
