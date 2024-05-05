a41e7e062ff08b3e30cdaf46daf10c033f68c9cd
Abobakr <mabobakr79@yahoo.com>
2024-05-05 21:21:53 +0300

    Initial Commit for 'router' project

---
+++ router/src/main/java/app/router/DeviceClass.java
@@ -0,0 +1,5 @@
+package app.router;
+
+public class DeviceClass {
+    
+}
---
+++ router/src/main/java/app/router/NetworkClass.java
@@ -0,0 +1,8 @@
+package app.router;
+
+public class NetworkClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
