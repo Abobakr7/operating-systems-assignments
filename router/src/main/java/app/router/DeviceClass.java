5303161497a60ac20cd51b3218774e7c2870209e
Abobakr <mabobakr79@yahoo.com>
2024-06-01 18:35:53 +0300

    Built DeviceClass and SemaphoreClass classes in router project

--- router/src/main/java/app/router/DeviceClass.java
+++ router/src/main/java/app/router/DeviceClass.java
@@ -1,5 +1,41 @@
 package app.router;
 
-public class DeviceClass {
+public class DeviceClass implements Runnable {
+    private final String name;
+    private final String type;
+    private int turn;
+    private final RouterClass router;
+    
+    public DeviceClass(String name, String type, RouterClass router) {
+        this.name = name;
+        this.type = type;
+        this.router = router;
+    }
+    
+    private void login() {
+        System.out.println("Connection " + this.turn + ": " + this.name + " logged in\n");
+    }
+    
+    private void preformActivity() throws InterruptedException {
+        Thread.sleep(2000);
+        System.out.println("Connection " + this.turn + ": " + this.name + " performing online activity\n");
+        Thread.sleep(2000);
+    }
+    
+    private void logout() {
+        System.out.println("Connection: " + this.turn + ": " + this.name + " logged out\n");
+//        this.router.disconnect(name);
+    }
+    
+    @Override
+    public void run() {
+        try {
+            this.login();
+            this.preformActivity();
+            this.logout();
+        } catch (InterruptedException ex) {
+            Thread.currentThread().interrupt();
+        }
+    }
     
 }
--- router/src/main/java/app/router/SemaphoreClass.java
+++ router/src/main/java/app/router/SemaphoreClass.java
@@ -1,5 +1,31 @@
 package app.router;
 
 public class SemaphoreClass {
+    private int val;
     
+    public SemaphoreClass() {
+        this.val = 0;
+    }
+    
+    public SemaphoreClass(int val) {
+        this.val = val;
+    }
+    
+    public synchronized void reserve(String name, String type) throws InterruptedException {
+        --this.val;
+        if (this.val < 0) {
+            System.out.println(name + " (" + type + ") arrived and waiting\n");
+            wait();
+        } else {
+            System.out.println(name + " (" + type + ") arrived\n");
+        }
+    }
+    
+    public synchronized void release(String name, int deviceTurn) {
+        ++this.val;
+        if (this.val <= 0) {
+            notify();
+        }
+        System.out.println("Connection " + deviceTurn + ": " + name + " logged out\n");
+    }
 }
